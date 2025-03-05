644af41a77eaaf224c7823e7010a6bb194f390a9
Cpjr446 <chandraprakashjr446@gmail.com>
2025-03-05 12:37:12 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import time
+
+def main():
+    st.set_page_config("Information Retrieval")
+    st.header("Information-Retrieval-System 🤖")
+
+    user_query = st.text_input("Enter your query here:")
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader(
+            "Upload your PDF Files and Click on the Submit & Process Button", 
+            accept_multiple_files=True
+        )
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                vector_store = get_vector_store(text_chunks)
+                st.session_state.conversation = get_conversational_chain(vector_store)
+            st.success("Done")
+
+    
+
+    # Initialize session state variables
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chatHistory" not in st.session_state:
+        st.session_state.chatHistory = None
+
+  
+
+if __name__ == "__main__":
+    main()
